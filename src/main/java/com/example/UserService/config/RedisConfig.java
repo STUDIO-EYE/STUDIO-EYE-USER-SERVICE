1521eb683952aead825329dfd66bc6fc3e733aab
강승우 <phonil@naver.com>
2024-04-05 01:46:20 +0900

    [FEAT]: 추가된 유저 api 배포

--- src/main/java/com/example/UserService/config/RedisConfig.java
+++ src/main/java/com/example/UserService/config/RedisConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
@@ -19,9 +20,16 @@ public class RedisConfig {
     @Value("${spring.data.redis.port}")
     private int redisPort;
 
+    @Value("${spring.data.redis.password}") // Add this line to retrieve password from properties
+    private String redisPassword; // Add this line to store password
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(redisHost, redisPort);
+//        return new LettuceConnectionFactory(redisHost, redisPort);
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(redisHost, redisPort);
+        redisStandaloneConfiguration.setPassword(redisPassword); // Use password
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
+
     }
 
     @Bean
