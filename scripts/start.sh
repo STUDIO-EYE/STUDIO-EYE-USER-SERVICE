52831dad9e2421ce0508047521c3b66e144a4dff
강승우 <phonil@naver.com>
2024-04-14 04:18:48 +0900

    [FIX]: 경로 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-PROJECT_ROOT="/home/ubuntu/app/STUDIO-EYE-USER-SERVICE"
+PROJECT_ROOT="/home/ubuntu/app"
 JAR_FILE="$PROJECT_ROOT/spring-webapp.jar"
 
 APP_LOG="$PROJECT_ROOT/application.log"
